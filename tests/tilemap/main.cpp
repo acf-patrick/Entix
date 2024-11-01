dec174838ad54ddfda557752c5fb612a817835c6
acf-patrick <miharisoap@gmail.com>
2024-11-01 19:06:05 +0300

    simplify tilemap example

--- tests/tilemap/main.cpp
+++ tests/tilemap/main.cpp
@@ -5,27 +5,8 @@ class CustomHook : public entix::core::ApplicationHook {
 
    public:
     void startup() override {
-        eventListener
-            .listen(entix::core::Input::Event::QUIT,
-                    [&] { entix::core::Application::Quit(); })
-            .listen(entix::core::Input::Event::KEY_DOWN, [] {
-                const float speed = 2;
-                auto& scene = entix::core::SceneManager::Get()->getActive();
-                if (auto camera = scene.getEntity("main camera"); camera) {
-                    auto& cameraPosition =
-                        camera->get<entix::ecs::component::Transform>()
-                            .position;
-
-                    if (entix::core::Input::isKeyPressed(SDL_SCANCODE_UP))
-                        cameraPosition.y -= speed;
-                    if (entix::core::Input::isKeyPressed(SDL_SCANCODE_DOWN))
-                        cameraPosition.y += speed;
-                    if (entix::core::Input::isKeyPressed(SDL_SCANCODE_LEFT))
-                        cameraPosition.x -= speed;
-                    if (entix::core::Input::isKeyPressed(SDL_SCANCODE_RIGHT))
-                        cameraPosition.x += speed;
-                }
-            });
+        eventListener.listen(entix::core::Input::Event::QUIT,
+                             [&] { entix::core::Application::Quit(); });
     }
 };
 
