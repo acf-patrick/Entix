341d3a674a2bb48a5db5f71565ac7a3edabf8cdb
acf-patrick <miharisoap@gmail.com>
2024-11-24 21:18:52 +0300

    add LazyTimer

--- src/util/timer/timer.h
+++ src/util/timer/timer.h
@@ -2,20 +2,19 @@
 
 #include <SDL2/SDL.h>
 
+#include <optional>
+
 namespace entix {
 namespace util {
 
 class Timer {
     Uint32 _startTick;
     Uint32 _initialDelay, _delay;
-    bool _running;
+    bool _running = true;
 
    public:
-    Timer(Uint32 delay, bool running = true)
-        : _startTick(SDL_GetTicks()),
-          _initialDelay(delay),
-          _delay(delay),
-          _running(running) {}
+    Timer(Uint32 delay)
+        : _startTick(SDL_GetTicks()), _initialDelay(delay), _delay(delay) {}
 
     Uint32 getElapsedTime() const { return SDL_GetTicks() - _startTick; }
 
@@ -63,6 +62,72 @@ class Timer {
         _initialDelay = delay;
         _delay = delay;
         _running = true;
+    }
+};
+
+// Does not start the timer immediatly after instanciation
+class LazyTimer {
+    std::optional<Timer> _timer;
+    Uint32 _delay;
+
+   public:
+    LazyTimer(Uint32 delay) : _delay(delay) {}
+
+    Uint32 getElapsedTime() const {
+        if (_timer) return _timer->getElapsedTime();
+        return 0;
+    }
+
+    Uint32 getRemainingTime() const {
+        if (_timer) return _timer->getRemainingTime();
+        return _delay;
+    }
+
+    /**
+     * returns progress as a number between 0 and 1
+     */
+    float getProgress() const {
+        if (_timer) return _timer->getProgress();
+        return 0.0;
+    }
+
+    bool isDone() const {
+        if (_timer) return _timer->isDone();
+        return false;
+    }
+
+    bool isRunning() const {
+        if (_timer) return _timer->isRunning();
+        return false;
+    }
+
+    bool isPaused() const {
+        if (_timer) return _timer->isPaused();
+        return true;
+    }
+
+    void pause() {
+        if (_timer) _timer->pause();
+    }
+
+    void resume() {
+        if (_timer)
+            _timer->resume();
+        else
+            _timer = Timer(_delay);
+    }
+
+    void reset() {
+        if (_timer) _timer->reset();
+    }
+
+    void reset(Uint32 delay) {
+        if (_timer)
+            _timer->reset(delay);
+        else {
+            _timer = Timer(delay);
+            _delay = delay;
+        }
     }
 };
 
